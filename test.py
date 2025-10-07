ca8ac00885fcbafb34d734d73b1c379b0cb89dbd
laurelautissot-afk <laure.lau.tissot@gmail.com>
2025-10-07 15:18:10 -0400

    Debugged the dataset
    Removed all the strings from the dataset since the program had difficulty unpacking and procesing it

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import numpy as np
 
-gender, age, occupation, sleep_duration, quality_sleep, physical_activity, stress_levels, BMI, blood_pressure = np.loadtxt("Sleep_health_and_lifestyle_dataset.csv" , usecols = (2, 3, 4, 5, 6, 7, 8, 9, 10), unpack=True)
+Age, Sleep_duration, Quality_sleep, Physical_activity, Stress_levels = np.loadtxt("Sleep_health_and_lifestyle_dataset.csv" ,  skiprows=1, usecols=(2, 4, 5, 6, 7), unpack=True, delimiter=',')
