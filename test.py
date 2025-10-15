ac5183beb1c4199be5d27e16a134df10fe189894
mariellesul <mariellesuloukji@gmail.com>
2025-10-15 18:35:39 -0400

    Organized the comments

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 ############################### PART 3 #############################################################################################################################################
 
-#Changed how we imported our data so we can import strings as well
+#Changed how we imported our data (so we can import strings as well)
 import pandas as pd
 data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
-# These will be the variables that we will use, that are intresting (When putting _ was not working)
+# These will be the variables that we will use, that are intresting # Note: (When putting _ was not working)
 # This is the uniltered data
 print(data["Gender"], data["Age"], data["Sleep Duration"], data["Quality of Sleep"], data["Physical Activity Level"], data["Stress Level"], data["BMI Category"], data["Blood Pressure"], data["Sleep Disorder"])
 
@@ -11,7 +11,7 @@ print(data["Gender"], data["Age"], data["Sleep Duration"], data["Quality of Slee
 # Filter data: apply filter using loops and conditionals: We will only look at males
 # Create an empty list to add all roles where the gender is male.
 filtered_rows = []
-#  Data iterrows is a pandas DataFrame method that lets you loop (iterate) through each row of your DataFrame one by one.
+# Note: Data iterrows is a pandas DataFrame method that lets you loop (iterate) through each row of your DataFrame one by one.
 for i, row in data.iterrows():
     if row["Gender"] == "Male":
         filtered_rows.append(row)
@@ -24,22 +24,20 @@ print(filtered_data)
 import matplotlib.pyplot as plt
 
 ############################### C #####################################################################################################################################################################
-# Import a bar graph: Sleep duration Vs. Occupation, to analyse their relationship.
-#The relationship between occupation and sleep is important because different jobs can affect sleep duration. Job demands can influence how much rest people get. So important to analyse the realationship.
-# Group data by Occupation and calculate average Sleep duration (aka mean)
-avg_sleep = data.groupby("Occupation")["Sleep Duration"].mean()
-# Create a bar chart: with diffrent colors and line style # Each bar represents an occupation (from index) The height of the bar is the corresponding average sleep duration (from values).
+# Bar Graph:Sleep duration Vs. Occupation, to analyse their relationship.
+# To organize and visualize the relationship between occupation and sleep duration. (This is to see if different jobs can affect sleep duration. Job demands can influence how much rest people get. So it is important to analyse this realationship.)
+avg_sleep = data.groupby("Occupation")["Sleep Duration"].mean() # Note: Group data by Occupation and calculate average Sleep duration (aka mean)
+# Create a bar chart: with diffrent colors and line style # Note: Each bar represents an occupation (from index) The height of the bar is the corresponding average sleep duration (from values).
 plt.bar(avg_sleep.index, avg_sleep.values, color=["pink", "purple"], edgecolor = "red")
 plt.xlabel("Occupation")
 plt.ylabel("Average Sleep Duration (Hours)")
 plt.title("Average Sleep Duration by Occupation (Males Only)")
-# Used rotation to space out the names of the occupations on the x axis, if not they will go on top of each other. 
-plt.xticks(rotation=90)
-plt.show() #like print
+plt.xticks(rotation=90) # Note: Used rotation to space out the names of the occupations on the x axis, if not they will go on top of each other. 
+plt.show() # Note: Like print
 
 ############################### D ######################################################################################################################################################################
-#Scatter plot: Sleep Duration vs Quality of Sleep (Males Only) with grid
-#The scatter plot shows how sleep duration relates to sleep quality for males. It helps identify trends, such as whether longer or shorter sleep affects quality.
+# Scatter plot: Sleep Duration vs Quality of Sleep (Males Only) with grid
+# To visualy organize and  shows how sleep duration relates to sleep quality for males. (It helps identify trends, such as whether longer or shorter sleep affects quality.)
 plt.scatter(filtered_data["Sleep Duration"], filtered_data["Quality of Sleep"], color="pink", edgecolor='red')
 plt.xlabel("Sleep Duration (Hours)")
 plt.ylabel("Quality of Sleep")
@@ -48,12 +46,10 @@ plt.grid()
 plt.show()
 
 ############################## E #####################################################################################################################################################################
-#Two subplots side by side in one figure
-#Example: Compare "Age  vs Sleep Duration" and "Age vs Stress Level" (Males Only)
-#This shows the realation between how sleep duration changes with age and how stress levels change with age as well.
-fig, axes = plt.subplots(1, 2) # this creates single figure that has teo sublots (Axes) in it.1 = row 2 = collums.
-#Have to add axe and set to diffrenetiat between them ( because each suplot has its own SET of axes)
-# Subplot 1 (axe 0): Age Vs Sleep Duration (want scatter)
+#Two subplots side by side in one figure: Compare "Age  vs Sleep Duration" and "Age vs Stress Level" (Males Only)
+#To visualy organise and shows the realation between sleep duration and age. Also, how stress levels change with age as well.
+fig, axes = plt.subplots(1, 2) # Note: this creates single figure that has two sublots (Axes) in it ex: 1 = row 2 = collums.
+# Subplot 1 (axe 0): Age Vs Sleep Duration (want scatter) # Note: We have to add axe and set to tell the diffrence between them (because each suplot has its own SET of axes)
 axes[0].scatter(filtered_data["Age"], filtered_data["Sleep Duration"], color="magenta")
 axes[0].set_title("Age vs Sleep Duration (Males)")
 axes[0].set_xlabel("Age")
@@ -65,8 +61,8 @@ axes[1].set_xlabel("Age")
 axes[1].set_ylabel("Stress Level")
 
 ############################# F ######################################################################################################################################################################
-# Scatter plot: Physical activity Vs. Stress levels. 
-#To see the realationship between the two and if performing more physical activity improves stress, which therfore impoves sleep.
+#Scatter plot: Physical activity Vs. Stress levels. 
+#To visualy organize and see the realationship between Physical activity and Stress levels. To determine if performing more physical activity improves stress, which therfore impoves sleep.
 plt.scatter(filtered_data["Physical Activity Level"], filtered_data["Stress Level"], color="pink", edgecolor="red")
 plt.xlabel("Physical Activity Level")
 plt.ylabel("Stress Level")
